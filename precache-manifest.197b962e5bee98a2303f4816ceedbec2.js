57dc43caf8e2b89215e658dea204a887b47fab84
annyup <x-annypham@live.com>
2020-06-22 10:08:20 -0400

    Updates

---
+++ precache-manifest.197b962e5bee98a2303f4816ceedbec2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a48cd990551df407fc82a58daf0f771c",
+    "url": "/my-fake-react-app/index.html"
+  },
+  {
+    "revision": "c89f3bdc1623473f5470",
+    "url": "/my-fake-react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c506a3d3eb0b603dd162",
+    "url": "/my-fake-react-app/static/js/2.933de957.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-fake-react-app/static/js/2.933de957.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c89f3bdc1623473f5470",
+    "url": "/my-fake-react-app/static/js/main.7ab3c223.chunk.js"
+  },
+  {
+    "revision": "ab5bf20072a2e5fe7f02",
+    "url": "/my-fake-react-app/static/js/runtime-main.fd84fc22.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-fake-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
